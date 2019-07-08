b91a2d01374b795aa81ca1977091dfaf12f0d8ff
張仲豪 <goodjason@zhangzhonghaode-MacBook-Pro.local>
2019-07-09 00:13:17 +0800

    [Init] 全域 scss 變數設定

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
   </div>
 </template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss">
+.about {
+  h1 {
+    color: $color-yellow;
+  }
+}
+</style>
