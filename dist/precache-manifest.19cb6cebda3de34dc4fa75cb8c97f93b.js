a0ef777dba77a501b2984653bf1d0bed128783f0
張仲豪 <goodjason@zhangzhonghaode-MacBook-Pro.local>
2019-07-13 16:18:05 +0800

    [deploy]

---
+++ dist/precache-manifest.19cb6cebda3de34dc4fa75cb8c97f93b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfff488faba2a0bc2a30581bf348c164",
+    "url": "/TheF2E_Week1_Pomodoro/dist/index.html"
+  },
+  {
+    "revision": "6ed049159025c436fddd",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/app.0f16b0fa.css"
+  },
+  {
+    "revision": "e9d930f51134d4405574",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-32f8b599.65eb9764.css"
+  },
+  {
+    "revision": "e9d930f51134d4405574",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-32f8b599.d26b0bfe.js"
+  },
+  {
+    "revision": "73f24759c14a0b4c0b84",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-452c4894.f6357ff6.css"
+  },
+  {
+    "revision": "73f24759c14a0b4c0b84",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-452c4894.a2bfa89f.js"
+  },
+  {
+    "revision": "f130489ad95084b9f870",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-6073a108.3ad9f86d.css"
+  },
+  {
+    "revision": "f130489ad95084b9f870",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-6073a108.2d67e66a.js"
+  },
+  {
+    "revision": "e34050c40bfb4ae76477",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-7f4e3d90.58666711.css"
+  },
+  {
+    "revision": "e34050c40bfb4ae76477",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-7f4e3d90.5b1a4c09.js"
+  },
+  {
+    "revision": "230dfa3edac1c9c026b5",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-c4939264.294cdf51.js"
+  },
+  {
+    "revision": "7cb959901cdee2e7e64d",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-d91dda1e.4dd87e7f.css"
+  },
+  {
+    "revision": "7cb959901cdee2e7e64d",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-d91dda1e.e22ded44.js"
+  },
+  {
+    "revision": "737d972d850a5dbc6abd",
+    "url": "/TheF2E_Week1_Pomodoro/dist/css/chunk-vendors.acd9854f.css"
+  },
+  {
+    "revision": "737d972d850a5dbc6abd",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/chunk-vendors.b0f4c3e5.js"
+  },
+  {
+    "revision": "6ed049159025c436fddd",
+    "url": "/TheF2E_Week1_Pomodoro/dist/js/app.61c4a68d.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TheF2E_Week1_Pomodoro/dist/robots.txt"
+  },
+  {
+    "revision": "783e1f9ffff323f7fedb06e5e4d97493",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/warning.mp3"
+  },
+  {
+    "revision": "6b2a53ee8ac3cfc5df2d10afc0f89b97",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/whistle.mp3"
+  },
+  {
+    "revision": "ebb9f2a989451b891d9d5a8a6df416df",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/digital.mp3"
+  },
+  {
+    "revision": "783482533d4f3a3b22727275247b526b",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/alert.mp3"
+  },
+  {
+    "revision": "ba873aa9d7aba290c725b24f9a2cbddf",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/drop.mp3"
+  },
+  {
+    "revision": "9391f1fef92ec849259c93044c28be00",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/default.mp3"
+  },
+  {
+    "revision": "0328f8c81620712329b69ae0e30f08c0",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/bell.mp3"
+  },
+  {
+    "revision": "0063e353c2c14f8b6316030298a882dd",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/beep.mp3"
+  },
+  {
+    "revision": "a68d46d0c4281b616e84ae4e159b2221",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/music.mp3"
+  },
+  {
+    "revision": "e75c80b232158a67a99889cb269d9036",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/ring.mp3"
+  },
+  {
+    "revision": "dd436e72bc3e7cbf5304af0d0c0c1a2d",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/horn.mp3"
+  },
+  {
+    "revision": "aebd98211e718d78e0ddd447987b6dfa",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/bird.mp3"
+  },
+  {
+    "revision": "b84fccdc8556af860c81922684d0e01e",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/alarm.mp3"
+  },
+  {
+    "revision": "cfb74f7009735e8cb133aabca755de9d",
+    "url": "/TheF2E_Week1_Pomodoro/dist/audio/alert/Bugle.mp3"
+  }
+];
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist/'
+    ? '/TheF2E_Week1_Pomodoro/dist/'
     : '/',
   css: {
     loaderOptions: {
